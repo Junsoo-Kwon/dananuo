ffae46f73a073ba0e0967b162cbe2886fe37404e
Junsoo-Kwon <97787599+Junsoo-Kwon@users.noreply.github.com>
2022-01-16 21:21:31 +0900

    Delete dananuo_back directory

--- dananuo_back/src/main/java/com/ssafy/dananuo/service/IListService.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.dananuo.service;
-
-import java.util.List;
-
-import com.ssafy.dananuo.dto.Goods;
-
-public interface IListService {
-	public List<Goods> getGoodsList() throws Exception;
-}
